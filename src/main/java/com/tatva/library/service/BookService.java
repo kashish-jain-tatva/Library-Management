d32fb3b36e66a6dcf8786471569e9c93dc998399
vaibhavdabhitatvasoft <vaibhav.dabhi@tatvasoft.com>
2024-04-18 17:34:07 +0530

    added test cases and implemented functionality

--- src/main/java/com/tatva/library/service/BookService.java
+++ src/main/java/com/tatva/library/service/BookService.java
@@ -1,8 +1,12 @@
 package com.tatva.library.service;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
 import com.tatva.library.entities.Book;
+import com.tatva.library.exception.ResourceNotFoundException;
 import com.tatva.library.repository.BookRepository;
 
 @Service
@@ -14,18 +18,35 @@ public class BookService {
 		this.bookRepository = bookRepository;
 	}
 
-	
 	public Book createBook(Book book) {
-		
-		Book book2 = Book.builder().name("IKIGAI").build();
+
+		Book book2 = Book.builder().id(1).name("IKIGAI").build();
 		bookRepository.save(book2);
 		return book2;
 	}
 
-
 	public String checkIn(Book book) {
-		
-		return null;
+		Book book2 = findBookById(book.getId());
+		LocalDateTime dateTime = LocalDateTime.now();
+		book2.setCheckIn(dateTime);
+		bookRepository.save(book);
+		return "checked in!!";
 	}
 
+	
+	public Book findBookById(int id) {
+		Optional<Book> book = bookRepository.findById(id);
+		if (book.isPresent()) {
+			return book.get();
+		} else {
+			throw new ResourceNotFoundException("book not found");
+		}
+	}
+
+	public String checkOut(int id) {
+		Book book1 = findBookById(id);
+		LocalDateTime dateTime = LocalDateTime.now();
+		book1.setCheckOut(dateTime);
+		return "checked Out!!";
+	}
 }
--- src/test/java/com/tatva/library/service/BookServiceTest.java
+++ src/test/java/com/tatva/library/service/BookServiceTest.java
@@ -3,7 +3,10 @@ package com.tatva.library.service;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.when;
+
+import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -13,16 +16,15 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.tatva.library.entities.Book;
+import com.tatva.library.exception.ResourceNotFoundException;
 import com.tatva.library.repository.BookRepository;
 
 @ExtendWith(MockitoExtension.class)
 public class BookServiceTest {
 
-	
-	
 	@InjectMocks
 	private BookService bookService;
-	
+
 	@Mock
 	private BookRepository bookRepository;
 
@@ -30,7 +32,7 @@ public class BookServiceTest {
 
 	@BeforeAll
 	public static void setup() {
-		book = Book.builder().name("IKIGAI").build();
+		book = Book.builder().id(1).name("IKIGAI").build();
 	}
 
 	@Test
@@ -38,16 +40,44 @@ public class BookServiceTest {
 		when(bookRepository.save(book)).thenReturn(book);
 		Book book1 = bookService.createBook(book);
 		assertNotNull(book1);
-		assertEquals("IKIGAI", "IKIGAI");
+		assertEquals("IKIGAI", book.getName());
+	}
+
+	@Test
+	public void checkin() {
+		Book book = Book.builder().id(2).name("IKIGAI").build();
+		when(bookRepository.findById(anyInt())).thenReturn(Optional.of(book));
+		String bookString = bookService.checkIn(book);
+		assertEquals("checked in!!", bookString);
+	}
+
+	@Test
+	public void checkInThrowsException() {
+		when(bookRepository.findById(anyInt())).thenReturn(Optional.empty());
+		assertThrows(ResourceNotFoundException.class, () -> {
+			bookService.checkIn(book);
+		});
+	}
+
+	@Test
+	public void findBookByIdThrowsException() {
+		assertThrows(ResourceNotFoundException.class, () -> {
+			bookService.findBookById(0);
+		});
+	}
+	
+	@Test void checkOut() {
+		Book book = Book.builder().id(2).name("IKIGAI").build();
+		when(bookRepository.findById(anyInt())).thenReturn(Optional.of(book));
+		String bookString = bookService.checkOut(anyInt());
+		assertEquals("checked Out!!", bookString);
 	}
 	
 	@Test
-	public void checkinThrowsException() {
-		
-		Book book = null;
-		assertThrows(NullPointerException.class,
-	            ()->{
-	            	bookService.checkIn(book);
-	            });
+	public void checkOutThrowsException() {
+		when(bookRepository.findById(anyInt())).thenReturn(Optional.empty());
+		assertThrows(ResourceNotFoundException.class, () -> {
+			bookService.checkOut(anyInt());
+		});
 	}
 }
