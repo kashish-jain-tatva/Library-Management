0762bf44c5c3a48a84c9b4e248da44449b25d270
vaibhavdabhitatvasoft <vaibhav.dabhi@tatvasoft.com>
2024-04-18 16:20:22 +0530

    Vaibhav

--- src/main/java/com/tatva/library/entities/User.java
+++ src/main/java/com/tatva/library/entities/User.java
@@ -6,10 +6,12 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import lombok.Builder;
 import lombok.Data;
 
 @Entity
 @Data
+@Builder
 public class User {
 
 	@Id
